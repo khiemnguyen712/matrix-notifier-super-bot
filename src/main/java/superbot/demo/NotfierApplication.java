f36ba745264e23ebbaa4dc5e5cb65d0b95fa1d31
suka <khiemnguyen.hye@gmail.com>
2025-05-06 19:57:21 +0700

    Initial commit

---
+++ src/main/java/superbot/demo/NotfierApplication.java
@@ -0,0 +1,13 @@
+package superbot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotfierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotfierApplication.class, args);
+	}
+
+}
